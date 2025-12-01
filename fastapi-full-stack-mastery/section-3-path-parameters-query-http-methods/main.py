2304d1626f3f2010b9dcca1a0010b831f1fca8dd
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-12-01 09:29:07 +0600

    path, parameter, http methods

---
+++ fastapi-full-stack-mastery/section-3-path-parameters-query-http-methods/main.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+
+data = [
+    {'id': 1, 'name': 'iphone17', 'price': 100},
+    {'id': 2, 'name': 'iphone18', 'price': 100},
+]
+
+
+# GEt Request
+# Read or Fetch ALL DAta
+@app.get('/')
+def home():
+    ''' home '''
+    return {"message": "Hello World"}
+
+
+@app.get('/product')
+async def all_products():
+    ''' home '''
+    return {"response": data}
+
+
+# Read or fetch single product
+@app.get("/product/{product_id}")
+async def single_product(product_id: int):
+    ''' get single product '''
+    product = next((item for item in data if item["id"] == product_id), None)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return {"response": product}
+
+
+# POST Request
+# Create or Insert Data
+@app.post("/product")
+async def create_product(new_product: dict):
+    ''' create product '''
+    data.append(new_product)
+    return {"response": new_product}
+
+
+# PUT Request
+# Update Complete Data
+@app.post("/product/{product_id}")
+async def update_product(product_id: int, updated_product: dict):
+    ''' create product '''
+    index = next((i for i, item in enumerate(data) if item["id"] == product_id), None)
+    if index is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    # replace the whole object
+    data[index] = updated_product
+    return {"response": updated_product}
+
+
+# Partial update
+@app.patch("/product/{product_id}")
+async def patch_product(product_id: int, partial_data: dict):
+    """Partial update for product"""
+    product = next((item for item in data if item["id"] == product_id), None)
+
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    # update only given keys
+    for key, value in partial_data.items():
+        product[key] = value
+
+    return {"response": product}
+
+
+# Delete resource
+@app.delete("/product/{product_id}")
+async def delete_product(product_id: int):
+    """Delete a product"""
+    index = next((i for i, item in enumerate(data) if item["id"] == product_id), None)
+
+    if index is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    deleted = data.pop(index)
+
+    return {"message": "Product deleted", "deleted": deleted}
